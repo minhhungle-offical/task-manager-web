3edb5cdbae93cb2773596bab2832014ed0ff3e56
minhhungle <minhhungle.offical@gmail.com>
2025-07-13 18:12:24 +0700

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000/api'
-//const baseURL = import.meta.env.VITE_API_BASE_URL
+// const baseURL = 'http://localhost:3000/api'
+const baseURL = import.meta.env.VITE_API_BASE_URL
 
 const axiosClient = axios.create({
   baseURL,
